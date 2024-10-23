f4b22f8633b39f12b07a4a780f07fedbaa6aea46
kaiser-lai <klai2027@kehillahstudent.org>
2024-10-23 10:45:40 -0700

    done

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-import turtle
 
-# YOUR CODE HERE
 
-turtle.exitonclick()
+# YOUR CODe Here
+from turtle import *
+
+for i in range(4):
+    forward(100)
+    left(90)
+
+forward(50)
+left(45)
+
+for i in range(4):
+    forward(71)
+    left(90)
+
+exitonclick()
